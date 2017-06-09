9940c37187b5520736c7f3aaeffe223e0fc8c3d0
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2017-06-09 17:40:54 -0300

    fpga_synt.py was rewritten with other functionality (not tested yet)

--- helpers/fpga_synt.py
+++ helpers/fpga_synt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #
-# FPGA Synt, generates files to make a Synthesis
-# Copyright (C) 2015-2017 INTI, Rodrigo A. Melo
+# FPGA Synt, do synthesis based on the project file of the vendor's tool
+# Copyright (C) 2017 INTI, Rodrigo A. Melo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,30 +17,18 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import argparse, yaml, os, sys, shutil, glob
+import argparse, glob, sys, os, tempfile
+from fpga_db import *
 
-bin_dir = os.path.dirname(os.path.abspath(__file__))
-if os.path.exists(bin_dir + '/../data'):
-   share_dir = bin_dir + '/..'
-   lib_dir   = bin_dir
-else:
-   share_dir = bin_dir + '/../share/fpga-helpers'
-   lib_dir   = share_dir
+###################################################################################################
+# Parsing the command line
+###################################################################################################
 
-sys.path.insert(0, lib_dir)
-from fpga_lib import *
-
-## Parsing the command line ###################################################
-
-version = 'FPGA Synt (FPGA Helpers) v' + getVersion(share_dir)
-
-boards = []
-getBoards(boards, share_dir)
+version = "fpga_synt is a member of FPGA Helpers v%s" % (fpga_db.version)
 
 parser = argparse.ArgumentParser(
-   prog='fpga_synt',
-   description='Generates files to make a Synthesis.',
-   epilog='Supported boards: ' + ', '.join(boards)
+   prog="fpga_synt",
+   description="Do synthesis based on the project file of the vendor's tool"
 )
 
 parser.add_argument(
@@ -50,85 +38,74 @@ parser.add_argument(
 )
 
 parser.add_argument(
-   'tool',
-   metavar='TOOLNAME',
-   nargs='?',
-   choices=['ise','vivado','quartus2','libero-soc','all'],
-   help='Name of the vendor tool to be used [ise|vivado|quartus2|libero-soc|all]'
+   '-t', '--task',
+   metavar = 'TASK',
+   choices = ['syn','imp','bit'],
+   default = 'bit',
+   help    = 'TASK to be executed (syn|imp|bit) [bit]'
 )
 
 parser.add_argument(
-   '-b', '--board',
-   metavar='BOARDNAME|BOARDFILE',
-   help='Name of a supported board or file (.yaml) of a new/custom board '
+   'file',
+   nargs   = '?',
+   metavar = 'PROJECT_FILE',
+   help    = 'PROJECT_FILE to be used [auto detected]'
 )
 
 options = parser.parse_args()
 
-## Processing the options #####################################################
+###################################################################################################
+# Collecting information
+###################################################################################################
 
-print ('fpga_synt (INFO): ' + version)
+# Detecting file project
 
-if options.tool is None:
-   print ("fpga_synt (INFO): you did not select tool to use. Choose one:")
-   print ("0. All")
-   print ("1. ISE (Xilinx)")
-   print ("2. Vivado (Xilinx)")
-   print ("3. Quartus2 (Altera)")
-   print ("4. Libero-SoC (Microsemi)")
-   option = sys.stdin.read(1)
-   if option == "0":
-      options.tool = "all"
-   elif option == "1":
-      options.tool = "ise"
-   elif option == "2":
-      options.tool = "vivado"
-   elif option == "3":
-      options.tool = "quartus2"
-   elif option == "4":
-      options.tool = "libero-soc"
-   else:
-      sys.exit('fpga_synt (ERROR): invalid option.')
+if options.file is None:
+   options.file = \
+      glob.glob("*.xise") + \
+      glob.glob("*.xpr")  + \
+      glob.glob("*.qpf")  + \
+      glob.glob("*.prjx")
+   try:
+      options.file = options.file[0]
+   except:
+      sys.exit("fpga_synt (ERROR): project file not auto detected")
 
-fpga_prog_text = "ifneq ($(shell which fpga_prog),)\n\n";
-if options.board is None:
-   fpga_prog_text += 'prog-fpga:\n\tfpga_prog --tool=$(TOOL)' + \
-                     ' --device=fpga --position=1 $(firstword $(BIT_FILE))\n'
+if not os.path.exists(options.file):
+   sys.exit("fpga_synt (ERROR): file %s do not exists" % options.file)
+
+# Setting vendor's tool
+
+options.tool = None
+if options.file.endswith('.xise'):
+   options.tool = 'ise'
+elif options.file.endswith('.xpr'):
+   options.tool = 'vivado'
+elif options.file.endswith('.qpf'):
+   options.tool = 'quartus'
+elif options.file.endswith('.prjx'):
+   options.tool = 'libero'
 else:
-   if options.board.endswith(".yaml"):
-      path = options.board
-   else:
-      path = share_dir + '/data/boards/' + options.board + '.yaml'
-   if os.path.exists(path):
-      board = yaml.load(open(path, 'r'))
-   else:
-      sys.exit('fpga_synt (ERROR): <board> ' + options.board + ' not exists.')
-   # fpga_prog alternatives
-   if options.tool != 'all':
-      for device in sorted(board):
-          if device == 'fpga':
-             fpga_prog_text += 'prog-fpga:\n\tfpga_prog --tool=$(TOOL) --board=' + \
-                               options.board + ' --device=fpga $(firstword $(BIT_FILE))\n'
-          if device == 'spi':
-             fpga_prog_text += 'prog-spi:\n\tfpga_prog --tool=$(TOOL) --board=' + \
-                               options.board + ' --device=spi  $(firstword $(BIT_FILE))\n'
-          if device == 'bpi':
-             fpga_prog_text += 'prog-bpi:\n\tfpga_prog --tool=$(TOOL) --board=' + \
-                               options.board + ' --device=bpi  $(firstword $(BIT_FILE))\n'
-fpga_prog_text += "\nendif\n";
+   sys.exit("fpga_synt (ERROR): unsupported vendor's tool")
 
-## Generating files ###########################################################
+###################################################################################################
+# Running
+###################################################################################################
 
-if options.tool != 'all':
-   shutil.copy(share_dir + '/data/tools/synthesis/options.tcl', '.')
-   shutil.copy(share_dir + '/data/tools/synthesis/' + options.tool + '.tcl', '.')
-   open(options.tool + '.tcl','a').write("\n# Generated with " + version)
-else:
-   for filename in glob.glob(share_dir + '/data/tools/synthesis/*.tcl'):
-       shutil.copy(filename, '.')
-       open(os.path.basename(filename),'a').write("\n# Generated with " + version)
-shutil.copy(share_dir + '/data/tools/synthesis/Makefile', '.')
-open('Makefile','a').write(fpga_prog_text)
-open('Makefile','a').write("\n# Generated with " + version)
+# Preparing a temporary Makefile
+temp = tempfile.NamedTemporaryFile()
+temp.write("#!/usr/bin/make\n")
+temp.write("TOOL=%s\n" % options.tool)
+temp.write("TASK=%s\n" % options.task)
+temp.write("TCLPATH=%s\n" % (os.path.dirname(os.path.abspath(__file__)) + "/../tcl"))
+temp.write("include $(TCLPATH)/Makefile")
+temp.flush()
 
-print ('fpga_synt (INFO): files were generated.')
+# Executing the Makefile
+try:
+   os.system("make -f %s run" % temp.name)
+except:
+   print("fpga_synt (ERROR): failed when run %s" % options.task)
+
+# The Makefile was destroyed
+temp.close()
