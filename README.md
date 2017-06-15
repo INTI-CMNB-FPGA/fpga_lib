3c0b6853b6200a8d03799b6cb8f8c722aa0b46f1
Rodrigo Alejandro Melo <rmelo@inti.gob.ar>
2017-06-15 15:46:45 -0300

    The project is now only licensed under the BSD-3 clause

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # FPGA Lib
 
-Library of HDL snippets (such as cores, procedures and functions) and Tcl scripts, commonly shared
-between FPGA projects.
-You can read [vhdl/README.md](vhdl/README.md) and [tcl/README.md](tcl/README.md) for more details.
+Library of HDL snippets (such as cores, procedures and functions) commonly shared between FPGA
+projects.
 
 This project is mainly developed and mantained by the *FPGA team* of the
 *Centre of Nano and Microelectronics* [CMNB](http://www.inti.gob.ar/microynanoelectronica/)
@@ -11,13 +10,10 @@ Contributions are welcome.
 
 * We try to develop portable code to be vendor independant.
 * The main used HDL language is VHDL (93) and we use [GHDL](http://ghdl.free.fr/) to simulate.
-* Verilog (2001) code and iVerilog (Icarus) support will be added.
-* Instead of run a lot of different programs from command line, we prefer Tcl because is a
-  programming language shared between vendors tools.
+  You can read [vhdl/README.md](vhdl/README.md) for more details.
+* Verilog (2001) code and iVerilog [Icarus](http://iverilog.icarus.com/) support will be added.
 * Documentation is under construction.
 
 ## License
 
-FPGA Lib as project and the Software part (Tcl and Python scripts) are licensed under the GPL 3.
-HDL code is licensed under the BSD 3-clause.
-See [LICENSE](LICENSE) and [LICENSE-BSD](LICENSE-BSD) for details.
+FPGA Lib is licensed under the BSD 3-clause. See [LICENSE](LICENSE) for details.
--- verilog/verif/blink.v
+++ verilog/verif/blink.v
@@ -1,24 +1,3 @@
-// File ../temp/blink_tmp.vhdl translated with vhd2vl v2.5 VHDL to Verilog RTL translator
-// vhd2vl settings:
-//  * Verilog Module Declaration Style: 2001
-
-// vhd2vl is Free (libre) Software:
-//   Copyright (C) 2001 Vincenzo Liguori - Ocean Logic Pty Ltd
-//     http://www.ocean-logic.com
-//   Modifications Copyright (C) 2006 Mark Gonzales - PMC Sierra Inc
-//   Modifications (C) 2010 Shankar Giri
-//   Modifications Copyright (C) 2002, 2005, 2008-2010, 2015 Larry Doolittle - LBNL
-//     http://doolittle.icarus.com/~larry/vhd2vl/
-//
-//   vhd2vl comes with ABSOLUTELY NO WARRANTY.  Always check the resulting
-//   Verilog for correctness, ideally with a formal verification tool.
-//
-//   You are welcome to redistribute vhd2vl under certain conditions.
-//   See the license (GPLv2) file included with the source for details.
-
-// The result of translation follows.  Its copyright status should be
-// considered unchanged from the original VHDL.
-
 //
 // Blink
 //
@@ -28,40 +7,34 @@
 // Copyright (c) 2015 Authors and INTI
 // Distributed under the BSD 3-Clause License
 //
-// no timescale needed
 
 module Blink(
-input wire clk_i,
-input wire rst_i,
-output wire blink_o
+   input  wire clk_i,
+   input  wire rst_i,
+   output wire blink_o
 );
 
 parameter [31:0] FREQUENCY=25E6;
 parameter [31:0] SECONDS=1;
 
-
-
 parameter DIV = FREQUENCY * SECONDS;
 reg blink;
 reg [$clog2(DIV)-1:0] cnt = 0;
 
-  always @(posedge clk_i) begin : P1
-
-    if(rst_i == 1'b1) begin
+always @(posedge clk_i) begin : P1
+   if (rst_i == 1'b1) begin
       cnt = 0;
       blink <= 1'b0;
-    end
-    else begin
-      if(cnt == DIV) begin
-        cnt = 0;
-        blink <=  ~((blink));
-      end
-      else begin
+   end else begin
+      if (cnt == DIV) begin
+         cnt = 0;
+         blink <=  ~((blink));
+      end else begin
         cnt = cnt + 1;
       end
-    end
-  end
+   end
+end
 
-  assign blink_o = blink;
+assign blink_o = blink;
 
 endmodule
